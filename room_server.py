4285ff5fffc8961c9d44372a648c6e6acd957f84
berkesoylum79-ux <berkesoylum79@gmail.com>
2026-03-17 23:07:26 +0300

    Update room_server.py

--- room_server.py
+++ room_server.py
@@ -25,7 +25,7 @@ CORS(app, origins="*")
 socketio = SocketIO(
     app,
     cors_allowed_origins="*",
-    async_mode="gevent",
+    async_mode="threading",
     ping_timeout=60,
     ping_interval=25,
 )
